b01e71a6065496eb16fcdd470f0f083e3d86a134
anushkavhatkar <vhata986@newschool.edu>
2023-03-28 04:30:48 -0400

    succesfully pulled all textile images from json file to html

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let placeholder = document.querySelector('#textile-container');
+let info = document.getElementsByTagName('section');
 
 fetch("https://jumbled-ludicrous-okapi.glitch.me/source_data.json")
 
@@ -9,7 +11,8 @@ fetch("https://jumbled-ludicrous-okapi.glitch.me/source_data.json")
 
 .then(function (textiles) {
 
-let placeholder = document.querySelector('#container');
+
+
 
 displayData(textiles);
 
@@ -19,8 +22,7 @@ function displayData(textiles) {
     for (let textile of textiles) {
         const itemElement = document.createElement("div");
 
-        itemElement.innerHTML = `<div class="textileimage"><img src='${textile.image}'></div>
-        <div class="${textile.name}">${textile.name +" - "+ textile.state}</div>`;
+        itemElement.innerHTML = `<div class="textile"><img src='${textile.image}'></div>`;
 
         placeholder.appendChild(itemElement);
     }
@@ -31,4 +33,19 @@ function displayData(textiles) {
 
 
 
+
 })
+
+let randomColors =
+[  "#b93254", "#af2642", "#472515", "#ffffff", "#b99a4d", "#484b67", "#23021a", "#d9d9d9", "#3d2024", "#0b1d26",  "#82676b", "#7c0208", "#ce7d39", "#bc672f", "#ebd9a9", "#e2d9c1", "#d0af84", "#b69a85", "#e0bf7c", "#dddcda",  "#9e2020", "#89211a", "#895d0d", "#a68433", "#bab1a8", "#84256a", "#e6dbc3", "#e8ded1", "#bc9c6f", "#a38334",  "#b3b3a9", "#867652", "#1d4d78", "#e8eadb", "#aaa9aa", "#bdb347", "#3a8489", "#e19a69", "#bb5326", "#b19d80",  "#a68e6d", "#ffffff", "#f0f0f0", "#db802f", "#a08455", "#050805", "#01031e", "#785d4e", "#545421", "#d9c967",  "#bbaf92", "#c7b199", "#877339", "#ce3b3f", "#1d488d", "#225198", "#68686d", "#701510", "#474648", "#142d8d",  "#b8977f", "#255aa1", "#85b8d5", "#5b575e", "#982f1a", "#2c2c2c", "#697cbe", "#4c946f", "#555659", "#2c0605",  "#252222"];
+
+const textileImages = document.querySelectorAll('textile');
+
+function randomFromArray(arr){
+    return arr[Math.floor(Math.random()*arr.length)]
+  }
+
+for (i=0; i<textileImages.length; i++) {
+
+    textileImages[i].style.borderColor = randomFromArray(randomColors);
+}
